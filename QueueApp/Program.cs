679b4b7d9edf6d063243f19f32c7545075b47b7e
Radoslav H. Todorov <rht@templafy.com>
2021-02-14 12:39:46 +0100

    New Console app

---
+++ QueueApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace QueueApp
+{
+    class Program
+    {
+        private const string ConnectionString = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=mslearnazuremessaging;AccountKey=N8h8aLahpSdEGL2OiP1sh3IdlDmo2NAbAqSuHRhX48eeRxpGZUVLnQUNBudJYJfllY9N2YrDuLoCQw9OYKyOxA==";
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
